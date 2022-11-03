11f365fa912e7ed3b2e016bf5bea5ade56ef8e08
HadadArdianto <ardiansracing@gmail.com>
2022-11-04 07:07:33 +0900

    first commit

---
+++ assets/source.js
@@ -0,0 +1,12 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function buy(product_name, product_price) {
+  const message = `Halo saya mau beli ${product_name} - ${product_price}`
+  const whatsapp = `https://wa.me/6282238014932?text=${message}`
+  open(whatsapp, "_blank")
+}
+
+function chat() {
+  alert('Halo min saya mau tanya nih...');
+}
+
